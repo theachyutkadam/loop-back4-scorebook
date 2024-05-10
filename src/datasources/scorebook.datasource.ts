8688798dfb6821e303c15dda7564a3cf6090ecd2
theachyutkadam <theachyutkadam@gmail.com>
2024-05-10 13:56:14 +0530

    Set database source and connection

---
+++ src/datasources/scorebook.datasource.ts
@@ -0,0 +1,31 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'scorebook',
+  connector: 'postgresql',
+  url: '',
+  host: '127.0.0.1',
+  port: 5432,
+  user: 'postgres',
+  password: 'password',
+  database: 'scorebook_development'
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class ScorebookDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'scorebook';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.scorebook', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
